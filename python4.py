abe7d9cdcae4a88e86be2831622829324c84ec08
MEXGodos <bryangodos7788@gmail.com>
2023-09-11 15:45:04 -0500

    Another Personal Commit I Guess, but this time 4

---
+++ python4.py
@@ -0,0 +1,15 @@
+name = []
+code = []
+
+with open("data.csv","r") as names_csv:
+    lines = names_csv.readlines()
+
+    for i in lines:
+        new_list = i.split(",")
+        name.append(new_list[0])
+        code.append(new_list[1])
+
+    for i in range(len(name)):
+        print("-------------------------------------------------------------------------------------------------------------------")
+        print(name[i])
+        print(code[i])
